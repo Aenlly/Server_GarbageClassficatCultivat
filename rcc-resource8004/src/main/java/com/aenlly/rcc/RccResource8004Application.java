d7835d8439cc06e4e6ca3a991de5108d08ef8b9f
Aenlly <2645274834@qq.com>
2021-12-13 12:57:04 +0800

    rcc-resource8004项目创建资源服务：用于资源调用

---
+++ rcc-resource8004/src/main/java/com/aenlly/rcc/RccResource8004Application.java
@@ -0,0 +1,12 @@
+package com.aenlly.rcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RccResource8004Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RccResource8004Application.class, args);
+  }
+}
